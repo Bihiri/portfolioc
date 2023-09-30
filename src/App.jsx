5feef185864e50f44b856aa1b44fbd61a1167f23
Sorobi Bhiri <146157973+Bihiri@users.noreply.github.com>
2023-09-30 05:43:24 +0000

    personal details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Bihiri",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "bihiri@bi",
+  gitHub: "bihiri",
 };
 
 const primaryColor = "#4E567E";
